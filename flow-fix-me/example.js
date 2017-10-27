88ba259a1a00b31f6a9bb7844537670d844949e5
Pavel Zubkou <pavel.zubkou@gmail.com>
2017-10-27 12:33:31 +0300

    Add note that flow.org/try doesn't support suppression

---
+++ flow-fix-me/example.js
@@ -0,0 +1,7 @@
+// @flow
+
+// $FlowFixMe
+const a: string = 12;
+
+// flow.org/try doesn't support suppression
+// https://flow.org/try/#0PTAEAEDMBsHsHcBQiSgCQDE7wwSwB4CyApogMawB2AzgC6gCGAXKHQE66UDmoAvKAEYATAG5EQA
